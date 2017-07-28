91d20d11d797dbb4e8607166f3ab26721bfe650e
hyperreality <lmt@fastmail.com>
2017-07-28 23:46:33 +0100

    Added two more converters

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var iota = new IOTA();
+
 function bitsToTrits(bits) {
   return bits * Math.log(2) / Math.log(3);
 }
@@ -8,25 +10,60 @@ function tritsToBits(trits) {
 
 function bitsToTritsConv(field) {
   switch (field.id) {
-    case 'bytes':
-      var bytes = true;
-    case 'bits':
-      var bits = bytes ? field.value * 8 : field.value;
-      document.getElementById('bits').value = bits;
-      document.getElementById('bytes').value = bits / 8;
-      document.getElementById('trits').value = bitsToTrits(bits);
-      document.getElementById('trytes').value = bitsToTrits(bits) / 3;
-      bytes = false;
+    case 'bits_bytes':
+      var flag = true;
+    case 'bits_bits':
+      var bits = flag ? field.value * 8 : field.value;
+      document.getElementById('bits_bits').value = bits;
+      document.getElementById('bits_bytes').value = bits / 8;
+      document.getElementById('bits_trits').value = bitsToTrits(bits);
+      document.getElementById('bits_trytes').value = bitsToTrits(bits) / 3;
+      flag = false;
       break;
-    case 'trytes':
-      var trytes = true;
-    case 'trits':
-      var trits = trytes ? field.value * 3 : field.value;
-      document.getElementById('bits').value = tritsToBits(trits);
-      document.getElementById('bytes').value = tritsToBits(trits) / 8;
-      document.getElementById('trits').value = trits;
-      document.getElementById('trytes').value = trits / 3;
-      trytes = false;
+    case 'bits_trytes':
+      var flag = true;
+    case 'bits_trits':
+      var trits = flag ? field.value * 3 : field.value;
+      document.getElementById('bits_bits').value = tritsToBits(trits);
+      document.getElementById('bits_bytes').value = tritsToBits(trits) / 8;
+      document.getElementById('bits_trits').value = trits;
+      document.getElementById('bits_trytes').value = trits / 3;
+      flag = false;
+      break;
+  }
+}
+
+function base10Conv(field) {
+  switch (field.id) {
+    default:
+    case 'base10_base10':
+      var tritsArray = fromValue(field.value);
+      if (document.getElementById('base10_pad').checked) {
+        while (tritsArray.length % 3 != 0)
+          tritsArray.push(0);                         
+      }
+      document.getElementById('base10_trits').value = tritsArray.toString();
+      document.getElementById('base10_trytes').value = trytes(tritsArray);
+      break;
+    case 'base10_trits':
+      var arr = field.value.split(',').map(Number);
+      document.getElementById('base10_base10').value = value(arr);
+      document.getElementById('base10_trytes').value = trytes(arr);
+      break;
+    case 'base10_trytes':
+      document.getElementById('base10_base10').value = value(trits(field.value));
+      document.getElementById('base10_trits').value = trits(field.value).toString();
+      break;
+  }
+}
+
+function asciiConv(field) {
+  switch (field.id) {
+    case 'ascii_ascii':
+      document.getElementById('ascii_trytes').value = iota.utils.toTrytes(field.value);
+      break;
+    case 'ascii_trytes':
+      document.getElementById('ascii_ascii').value = iota.utils.fromTrytes(field.value);
       break;
   }
 }
